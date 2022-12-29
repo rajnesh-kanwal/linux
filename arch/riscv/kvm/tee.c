5bd01491078c204243fbab0c8dabebb1b8bd1bb0
Atish Patra <atishp@rivosinc.com>
2022-12-29 09:55:51 -0800

    RISC-V: KVM: Add gstage mapping for TVMs
    For TVM, the gstage mapping is managed by the TSM via TEEH SBI
    calls. Implement those mappings.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- arch/riscv/kvm/tee.c
+++ arch/riscv/kvm/tee.c
@@ -115,11 +115,73 @@ void kvm_riscv_tee_vcpu_put(struct kvm_vcpu *vcpu)
 	/* TODO */
 }
 
+/* Inspired from pkvm_mem_abort */
 int kvm_riscv_tee_gstage_map(struct kvm_vcpu *vcpu, gpa_t gpa, unsigned long hva)
 {
-	/* TODO */
+	struct kvm_riscv_tee_page *tpage;
+	struct mm_struct *mm = current->mm;
+	struct kvm *kvm = vcpu->kvm;
+	struct kvm_riscv_tee_page_range nc_pr;
+	//TODO: Do need a FOLL_HWPOISION like pkvm ?
+	unsigned int flags = FOLL_LONGTERM | FOLL_WRITE;
+	struct page *page;
+	int rc;
+	struct kvm_tee_tvm_context *tvmc = kvm->arch.tvmc;
+
+	tpage = kmalloc(sizeof(*tpage), GFP_KERNEL_ACCOUNT);
+	if (!tpage)
+		return -ENOMEM;
+
+	mmap_read_lock(mm);
+	rc = pin_user_pages(hva, 1, flags, &page, NULL);
+	mmap_read_unlock(mm);
+
+	//TODO: Do we need to handle -EHWPOISON here as well?
+	if (rc != 1) {
+		rc = -EFAULT;
+		goto free_tpage;
+	} else if (!PageSwapBacked(page)) {
+		rc = -EIO;
+		goto free_tpage;
+	}
+
+	nc_pr.num_pages = 1;
+	nc_pr.pgr_phys = page_to_phys(page);
+	nc_pr.pgr = page_to_virt(page);
+	nc_pr.ptype = kvm_tee_map_ptype(PAGE_SIZE);
+
+	//TODO: We can't do spin lock here it sends IPI to each vcpu to initiate local fence. 
+	rc = tee_convert_pages(&nc_pr, true);
+	if(rc)
+		goto unpin_page;
+
+	spin_lock(&kvm->mmu_lock);
+	rc = sbi_teeh_add_zero_pages(tvmc->tvm_guest_id, nc_pr.pgr_phys,
+				     nc_pr.ptype, 1, gpa);
+	if (rc) {
+		pr_err("%s: Adding zero pages failed %d\n", __func__, rc);
+		goto zero_page_failed;
+	}
+	tpage->page = page;
+	INIT_LIST_HEAD(&tpage->link);
+	list_add(&tpage->link, &kvm->arch.tvmc->zero_pages);
+
+	spin_unlock(&kvm->mmu_lock);
+
 	return 0;
+
+zero_page_failed:
+	spin_unlock(&kvm->mmu_lock);
+
+unpin_page:
+	unpin_user_pages(&page, 1);
+
+free_tpage:
+	kfree(tpage);
+
+	return rc;
 }
+
 
 void kvm_riscv_tee_vcpu_switchto(struct kvm_vcpu *vcpu, struct kvm_cpu_trap *trap)
 {
