3ae7147c5f20e6928814a52ea7307a1e7de551d7
Atish Patra <atishp@rivosinc.com>
2023-01-18 12:53:37 -0800

    RISC-V: KVM: Implement static memory region measurement
    To support attestion of any images loaded by the VMM, the TEEH allows
    measuring these memory regions. Currently, it will be used for the
    kernel image, device tree and initrd images.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- arch/riscv/include/asm/kvm_tee.h
+++ arch/riscv/include/asm/kvm_tee.h
@@ -131,6 +131,7 @@ void kvm_riscv_tee_vcpu_load(struct kvm_vcpu *vcpu);
 void kvm_riscv_tee_vcpu_put(struct kvm_vcpu *vcpu);
 void kvm_riscv_tee_vcpu_switchto(struct kvm_vcpu *vcpu, struct kvm_cpu_trap *trap);
 
+int kvm_riscv_tee_vm_measure_pages(struct kvm *kvm, struct kvm_riscv_tee_measure_region *mr);
 int kvm_riscv_tee_vm_add_memreg(struct kvm *kvm, unsigned long gpa, unsigned long size);
 int kvm_riscv_tee_gstage_map(struct kvm_vcpu *vcpu, gpa_t gpa, unsigned long hva);
 #else
@@ -149,6 +150,7 @@ static inline int kvm_riscv_tee_vcpu_init(struct kvm_vcpu *vcpu) {return -1;}
 static inline void kvm_riscv_tee_vcpu_load(struct kvm_vcpu *vcpu) {}
 static inline void kvm_riscv_tee_vcpu_put(struct kvm_vcpu *vcpu) {}
 static inline void kvm_riscv_tee_vcpu_switchto(struct kvm_vcpu *vcpu, struct kvm_cpu_trap *trap) {}
+static inline int kvm_riscv_tee_vm_measure_pages(struct kvm *kvm, struct kvm_riscv_tee_measure_region *mr) {return -1};
 static inline int kvm_riscv_tee_vm_add_memreg(struct kvm *kvm, unsigned long gpa, unsigned long size) {return -1;};
 static inline int kvm_riscv_tee_gstage_map(struct kvm_vcpu *vcpu,
 					   gpa_t gpa, unsigned long hva) {return -1;}
